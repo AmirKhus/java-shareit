564a4052a44045fff182064f78c94670b0e99f15
Amir <amri19040268@gmail.com>
2023-10-27 20:04:45 +0300

    мэппер для перевода типов данный с dto в модель и обратно

---
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
@@ -0,0 +1,28 @@
+package ru.practicum.shareit.item.dto;
+
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.User;
+
+public class ItemMapper {
+    public static ItemDto toItemDto(Item item) {
+        return ItemDto.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.getAvailable())
+                .userId(item.getUser().getId())
+                .build();
+    }
+
+    public static Item fromItemDto(ItemDto itemDto, User user, ItemRequest request) {
+        return Item.builder()
+                .id(itemDto.getId())
+                .name(itemDto.getName())
+                .description(itemDto.getDescription())
+                .available(itemDto.getAvailable())
+                .user(user)
+                .request(request)
+                .build();
+    }
+}
