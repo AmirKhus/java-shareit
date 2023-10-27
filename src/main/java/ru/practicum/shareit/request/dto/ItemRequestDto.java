ab3b0511ecab1f05ffff212d213df4aabd9d6e00
Amir <amri19040268@gmail.com>
2023-10-27 20:17:26 +0300

    dto для моделей

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,7 +1,19 @@
 package ru.practicum.shareit.request.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.user.User;
+
+import java.sql.Date;
+
 /**
  * TODO Sprint add-item-requests.
  */
+@Data
+@Builder
 public class ItemRequestDto {
+    private Integer id;
+    private String description;
+    private User requestor;
+    private Date created;
 }
