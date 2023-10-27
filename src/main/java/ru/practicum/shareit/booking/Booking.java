4ba3bb7344d53903fd270dd423328bb68549cce4
Amir <amri19040268@gmail.com>
2023-10-27 20:17:16 +0300

    модели

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,15 @@
 package ru.practicum.shareit.booking;
 
+import java.sql.Date;
+
 /**
  * TODO Sprint add-bookings.
  */
 public class Booking {
+    private Long id;
+    private Date start;
+    private Date stop;
+    private Long itemId;
+    private Long userId;
+    private Boolean isConfirmed;
 }
