fe96487e4297a7df27a352783da11741cb48d287
Amir <amri19040268@gmail.com>
2023-10-27 20:16:57 +0300

    dto для моделей

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,25 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.MarkerValidate;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@Builder
 public class ItemDto {
+    private Long id;
+    @NotBlank(groups = {MarkerValidate.OnCreate.class})
+    private String name;
+    @NotBlank(groups = {MarkerValidate.OnCreate.class})
+    private String description;
+    @NotNull(groups = {MarkerValidate.OnCreate.class})
+    private Boolean available;
+    private Long userId;
+    private Long requestId;
 }
