82a5b9a254b766faf88e523145efc72451d0447a
Amir <amri19040268@gmail.com>
2023-10-27 20:17:50 +0300

    контролеер

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,12 +1,57 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.MarkerValidate;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.service.ItemService;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * TODO Sprint add-controllers.
  */
+@Slf4j
 @RestController
 @RequestMapping("/items")
+@RequiredArgsConstructor
 public class ItemController {
+    private final ItemService itemService;
+    private static final String HEADER_USER_ID = "X-Sharer-User-Id";
+
+    @GetMapping
+    public List<ItemDto> getAllItemsByUser(@RequestHeader(HEADER_USER_ID) Long userId) {
+        return itemService.getItemByUser(userId);
+    }
+
+    @GetMapping("{itemId}")
+    public ItemDto getItemById(@PathVariable Long itemId) {
+        return itemService.getById(itemId);
+    }
+
+    @GetMapping("search")
+    public List<ItemDto> searchItems(@RequestParam String text) {
+        return itemService.search(text);
+    }
+
+    @PostMapping
+    public ItemDto createItem(@RequestHeader(HEADER_USER_ID) Long userId,
+                              @Validated({MarkerValidate.OnCreate.class}) @NotNull @RequestBody ItemDto itemDto) {
+        return itemService.create(itemDto, userId);
+    }
+
+    @PatchMapping("/{itemId}")
+    public ItemDto updateItem(@RequestHeader(HEADER_USER_ID) Long userId,
+                              @PathVariable Long itemId,
+                              @Validated(MarkerValidate.OnUpdate.class) @NotNull @RequestBody ItemDto itemDto) {
+        return itemService.updateItem(itemDto, itemId, userId);
+    }
+
+    @DeleteMapping("/{itemId}")
+    public ItemDto deleteItem(@PathVariable Long itemId) {
+        return itemService.delete(itemId);
+    }
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,47 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.MarkerValidate;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.service.UserService;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * TODO Sprint add-controllers.
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/users")
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        return userService.getAll();
+    }
+
+    @GetMapping("{id}")
+    public UserDto getUserById(@PathVariable Long id) {
+        return userService.getById(id);
+    }
+
+    @PostMapping
+    public UserDto createUser(@Validated(MarkerValidate.OnCreate.class) @NotNull @RequestBody UserDto userDto) {
+        return userService.create(userDto);
+    }
+
+    @PatchMapping("/{id}")
+    public UserDto updateUser(@PathVariable Long id,
+                              @Validated(MarkerValidate.OnUpdate.class) @NotNull @RequestBody UserDto userDto) {
+        return userService.update(id, userDto);
+    }
+
+    @DeleteMapping("/{userId}")
+    public UserDto deleteUser(@PathVariable Long userId) {
+        return userService.delete(userId);
+    }
 }
