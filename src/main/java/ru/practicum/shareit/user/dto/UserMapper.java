38ab15ef2aa3aeebcca33e2e7bc87a07278894f4
Amir <amri19040268@gmail.com>
2023-10-28 23:28:35 +0300

    удалена аннотация "@UtilityClass" из-за не прохода тест build в gitHub

--- src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
@@ -1,9 +1,6 @@
 package ru.practicum.shareit.user.dto;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import ru.practicum.shareit.user.User;
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserMapper {
     public static UserDto toUserDto(User user) {
         return UserDto.builder()
