158b172f371c90b26fdcb9848111b22edc6b6b27
Amir <amri19040268@gmail.com>
2023-11-17 00:39:08 +0300

    обновлены с поддержкой entity

--- src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
@@ -4,25 +4,68 @@ import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemMapper {
     public static ItemDto toItemDto(Item item) {
-        return ItemDto.builder()
+        ItemDto itemDto = ItemDto.builder()
                 .id(item.getId())
                 .name(item.getName())
                 .description(item.getDescription())
                 .available(item.getAvailable())
-                .userId(item.getUser().getId())
                 .build();
+
+        if (item.getRequest() != null) {
+            itemDto.setRequestId(item.getRequest().getId());
+        }
+
+        return itemDto;
     }
 
-    public static Item fromItemDto(ItemDto itemDto, User user, ItemRequest request) {
+    public static ItemDto toEntityItemDto(ru.practicum.shareit.item.entity.Item item) {
+        ItemDto itemDto = ItemDto.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.getAvailable())
+                .build();
+
+        if (item.getRequest() != null) {
+            itemDto.setRequestId(item.getRequest().getId());
+        }
+
+        return itemDto;
+    }
+
+    public static Item fromItemDto(ItemDto itemDto, User user) {
         return Item.builder()
                 .id(itemDto.getId())
                 .name(itemDto.getName())
                 .description(itemDto.getDescription())
                 .available(itemDto.getAvailable())
                 .user(user)
-                .request(request)
                 .build();
+    }
+
+
+
+    public static ru.practicum.shareit.item.entity.Item fromEntityItemDto(ItemDto itemDto, ru.practicum.shareit.user.entity.User user) {
+        return ru.practicum.shareit.item.entity.Item.builder()
+                .id(itemDto.getId())
+                .name(itemDto.getName())
+                .description(itemDto.getDescription())
+                .available(itemDto.getAvailable())
+                .user(user)
+                .build();
+    }
+
+    public static List<ItemDto> toItemDtoList(Iterable<ru.practicum.shareit.item.entity.Item> items) {
+        List<ItemDto> result = new ArrayList<>();
+
+        for (ru.practicum.shareit.item.entity.Item item : items) {
+            result.add(toEntityItemDto(item));
+        }
+        return result;
     }
 }
--- src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
@@ -2,8 +2,19 @@ package ru.practicum.shareit.user.dto;
 
 import ru.practicum.shareit.user.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserMapper {
     public static UserDto toUserDto(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+    }
+
+    public static UserDto toUserEntityDto(ru.practicum.shareit.user.entity.User user) {
         return UserDto.builder()
                 .id(user.getId())
                 .name(user.getName())
@@ -17,5 +28,22 @@ public class UserMapper {
                 .name(userDto.getName())
                 .email(userDto.getEmail())
                 .build();
+    }
+
+    public static ru.practicum.shareit.user.entity.User fromEntityUserDto(UserDto userDto) {
+        return ru.practicum.shareit.user.entity.User.builder()
+                .id(userDto.getId())
+                .name(userDto.getName())
+                .email(userDto.getEmail())
+                .build();
+    }
+
+    public static List<UserDto> toUserDtoList(Iterable<ru.practicum.shareit.user.entity.User> users) {
+        List<UserDto> result = new ArrayList<>();
+
+        for (ru.practicum.shareit.user.entity.User user : users) {
+            result.add(toUserEntityDto(user));
+        }
+        return result;
     }
 }
