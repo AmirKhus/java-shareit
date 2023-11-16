7a7224d6a14ca1fcf736df09d67dcf1f5fb7f81e
Amir <amri19040268@gmail.com>
2023-11-17 00:28:29 +0300

    контроллер обновлен

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,10 +1,66 @@
 package ru.practicum.shareit.booking;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.MarkerValidate;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingOutDto;
+import ru.practicum.shareit.booking.service.BookingService;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 
+@Slf4j
+@Validated
 @RestController
 @RequestMapping(path = "/bookings")
 public class BookingController {
+    @Autowired
+    private BookingService bookingService;
+    private static final String HEADER_USER_ID = "X-Sharer-User-Id";
+
+    @PostMapping()
+    public BookingOutDto addBookings(@RequestHeader(HEADER_USER_ID) Long userId,
+                                     @Validated(MarkerValidate.OnCreate.class) @NotNull @RequestBody BookingDto bookingDto) {
+        log.info("User {}, add new booking {}", userId, "bookingDto.getName()");
+        return bookingService.addBooking(bookingDto,userId);
+    }
+
+    @PatchMapping("/{bookingId}")
+    public BookingOutDto confirmationBooking(@RequestHeader(HEADER_USER_ID) Long userId,
+                                          @PathVariable Long bookingId,
+                                          @RequestParam(value = "approved") Boolean approved) {
+        log.info("User {}, changed the status booking {}", userId, bookingId);
+        return bookingService.confirmationBooking(userId, bookingId, approved);
+    }
+
+
+    @GetMapping("/{bookingId}")
+    public BookingOutDto getBookingById(@RequestHeader(HEADER_USER_ID) Long userId,
+                                        @PathVariable Long bookingId) {
+
+        log.info("Get booking {}", bookingId);
+        return bookingService.getBookingById(userId, bookingId);
+    }
+
+
+    @GetMapping
+    public List<BookingOutDto> getAllBrookingByBookerId(@RequestHeader(HEADER_USER_ID) Long userId,
+                                                       @RequestParam(value = "state", defaultValue = "ALL") String state) {
+        log.info("Get all bookings by booker Id {}", userId);
+        return bookingService.getAllBrookingByBookerId(userId, state);
+    }
+
+    @GetMapping("/owner")
+    public List<BookingOutDto> getAllBookingsForAllItemsByOwnerId(@RequestHeader(HEADER_USER_ID) Long userId,
+                                                                  @RequestParam(defaultValue = "ALL", required = false) String state) {
+
+        log.info("Get all bookings for all items by owner Id {}", userId);
+        return bookingService.getAllBookingsForAllItemsByOwnerId(userId, state);
+    }
+
 }
--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -2,9 +2,11 @@ package ru.practicum.shareit.item;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.MarkerValidate;
+import ru.practicum.shareit.comment.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.service.ItemService;
 
@@ -21,18 +23,24 @@ public class ItemController {
     private static final String HEADER_USER_ID = "X-Sharer-User-Id";
 
     @GetMapping
-    public List<ItemDto> getAllItemsByUser(@RequestHeader(HEADER_USER_ID) Long userId) {
-        return itemService.getItemByUser(userId);
+    public List<ItemDto> getAllItemsUser(@RequestHeader(HEADER_USER_ID) Long userId,
+                                         @RequestParam(required = false, defaultValue = "0") Integer from,
+                                         @RequestParam(required = false, defaultValue = "10") Integer size) {
+        log.info("List items User {}", userId);
+        return itemService.getItemsUser(userId,from,size);
     }
 
-    @GetMapping("{itemId}")
-    public ItemDto getItemById(@PathVariable Long itemId) {
-        return itemService.getById(itemId);
+    @GetMapping("/{itemId}")
+    public ItemDto getItemById(@RequestHeader(HEADER_USER_ID) Long userId,
+                               @PathVariable Long itemId) {
+        return itemService.getItemById(itemId, userId);
     }
 
     @GetMapping("search")
-    public List<ItemDto> searchItems(@RequestParam String text) {
-        return itemService.search(text);
+    public List<ItemDto> searchItems(@RequestParam String text,
+                                     @RequestParam(required = false, defaultValue = "0") Integer from,
+                                     @RequestParam(required = false, defaultValue = "10") Integer size) {
+        return itemService.search(text,from,size);
     }
 
     @PostMapping
@@ -51,5 +59,14 @@ public class ItemController {
     @DeleteMapping("/{itemId}")
     public void deleteItem(@PathVariable Long itemId) {
         itemService.delete(itemId);
+    }
+
+    @PostMapping("/{itemId}/comment")
+    public CommentDto addComment(@RequestHeader(HEADER_USER_ID) Long userId,
+                                                 @PathVariable Long itemId,
+                                                 @RequestBody CommentDto commentDto) {
+
+        log.info("User {} add comment for Item {}", userId, itemId);
+        return itemService.addComment(userId, itemId, commentDto);
     }
 }
