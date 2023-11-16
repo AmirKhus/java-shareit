dbc11edd2ca85c8aae8b07534171611c272d64d0
Amir <amri19040268@gmail.com>
2023-11-17 00:28:04 +0300

    Добавленые DTO

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,4 +1,31 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.MarkerValidate;
+import ru.practicum.shareit.booking.entity.Status;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class BookingDto {
+
+    private Long itemId;
+
+    @NotNull(groups = MarkerValidate.OnCreate.class)
+    private LocalDateTime start;
+
+    @NotNull(groups = MarkerValidate.OnCreate.class)
+    private LocalDateTime end;
+
+    private Status status;
 }
